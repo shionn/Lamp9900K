2d4e679f1ef7ac142cf69d70622a96a9638fcf04
Shionn <shionn@gmail.com>
2020-07-26 18:41:54 +0200

    Ajout de 2 mode

--- src/main.cpp
+++ src/main.cpp
@@ -45,40 +45,69 @@ void chenille(int step) {
 	}
 }
 
+void doubleChenille(int step) {
+	for(uint8_t i=0; i<PIXEL_COUNT; i++) {
+		uint8_t d = (step + PIXEL_COUNT - i) % (PIXEL_COUNT /2);
+		strip.setPixelColor(i, 255 >> d, 255 >> d, 255 >> d);
+	}
+}
+
+void randFlash(int state) {
+  if (!state) {
+    uint8_t pos = random(PIXEL_COUNT);
+    for(uint8_t i=0; i<PIXEL_COUNT; i++) {
+      if (pos == i && state == 0) {
+        strip.setPixelColor(i,255,255,255);
+      } else {
+        uint32_t color =  strip.getPixelColor(i);
+        strip.setPixelColor(i, 
+            color >> 17 , 
+            (color & 0x0000ff00) >> 9, 
+            (color & 0x000000ff) >> 1);
+      }
+    }
+  }
+}
+
 void loop() {
   int value = analogRead(A0);
   if (mode == 0) {
+    displayColor(strip.Color(0,0,0));
+  }
+  if (mode == 1) {
     uint8_t a = pow(2,value/128);
     displayColor(strip.Color(a,a,a));
   } 
-  if (mode == 1) {
+  if (mode == 2) {
     displayColor(Wheel(value/8));
   } 
-  if (mode == 2) {
+  if (mode == 3) {
 		uint8_t a = pow(2,abs(map(state, 0, 1023, -8, 8)));
-    state += value/16 + 1;
-    if ( state >= 1024) {
-      state -= 1024;
-    }
     displayColor(strip.Color(a,a,a));
   }
-  if (mode == 3) {
-    state += value/16 + 1;
-    if ( state >= 1024) {
-      state -= 1024;
-    }
+  if (mode == 4) {
     chenille(map(state, 0, 1024, 0, 39));
   }
-  if (mode == 4) {
-    displayColor(strip.Color(0,0,0));
+  if (mode == 5) {
+    doubleChenille(map(state, 0, 1024, 0, 39));
   }
+
+  if (mode == 6) {
+    randFlash(state%8);
+  }
+
+  state += value/16 + 1;
+  if ( state >= 1024) {
+    state -= 1024;
+  }
+
 
 
   strip.show();
   delay(10);
   if (digitalRead(BUTTON) == 0 && buttonState) {
     mode++;
-    if (mode == 5) {
+    if (mode == 7) {
       mode = 0;
     }
   }
